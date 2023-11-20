f05b484c784b67744c8d3fefe8b070fca2a854a9
Karina Timofeeva <kar.timofeeva93@gmail.com>
2023-11-20 18:16:58 +0100

    Updated src/index.js with format option

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import { Command } from 'commander';
 
+const program = new Command();
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .option('-V, --version', 'output the version number')
+  .option('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format')
+  .parse(process.argv);
+
+// чтобы получить наши options у программы(program) вызываем program.opts()
+// if help property is true =>
+// display the help information using program.helpInformation()
 const displayHelp = () => {
-  const program = new Command();
-
-  program
-    .description('Compares two configuration files and shows a difference.')
-    .option('-V, --version', 'output the version number')
-    .option('-h, --help', 'output usage information')
-    .parse(process.argv);
-
   if (program.opts().help) {
     console.log(program.helpInformation());
   }
