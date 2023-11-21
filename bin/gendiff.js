e5f3146b755403f9bc3804d89eaf45ca0ca1c522
Karina Timofeeva <kar.timofeeva93@gmail.com>
2023-11-21 17:26:39 +0100

    Updated bin/gendiff.js with filepath arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,4 +1,22 @@
 #!/usr/bin/env node
-import displayHelp from '../src/index.js';
+import { program } from 'commander';
+import { displayHelp, parseJSONFileIntoJSObject } from '../src/index.js';
 
-displayHelp();
+program
+  .description('Compares two configuration files and shows a difference.')
+  .option('-V, --version', 'output the version number')
+  .option('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    const displayParsedFiles = () => {
+      const file1 = parseJSONFileIntoJSObject(filepath1);
+      const file2 = parseJSONFileIntoJSObject(filepath2);
+      console.log(file1, file2);
+    };
+
+    displayParsedFiles();
+  })
+  .parse(process.argv);
+// put program in order to have access to the options of the Commander program:
+displayHelp(program);
