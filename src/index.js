07b728ab0e0dd041d6ec81cf0e6e3493ef47f708
Karina Timofeeva <kar.timofeeva93@gmail.com>
2023-11-22 11:42:30 +0100

    Updated src/index.js with compareJSON function

--- src/index.js
+++ src/index.js
@@ -24,3 +24,42 @@ export const parseJSONFileIntoJSObject = (filepath) => {
   const parsedFile = JSON.parse(contentOfJSONFile);
   return parsedFile;
 };
+
+export const compareTwoJSONFiles = (
+  filepath1,
+  filepath2,
+  replacer = ' ',
+  spacesCount = 1,
+) => {
+  const parsedFile1 = parseJSONFileIntoJSObject(filepath1);
+  const parsedFile2 = parseJSONFileIntoJSObject(filepath2);
+
+  const keys1 = Object.keys(parsedFile1);
+  const keys2 = Object.keys(parsedFile2);
+  const unitKeys = [...keys1, ...keys2];
+
+  const sortedUniqueKeys = [...new Set(unitKeys)].sort();
+  const space = replacer.repeat(spacesCount);
+  const spaceForEqualCase = replacer.repeat(spacesCount * 4);
+  const diff = sortedUniqueKeys.reduce((acc, key) => {
+    // check if values accociated with the keys are equal
+    if (parsedFile1[key] === parsedFile2[key]) {
+      acc.push(`${spaceForEqualCase}${key}: ${parsedFile1[key]}`);
+    } else if (
+      parsedFile1[key] !== undefined
+      && parsedFile2[key] === undefined
+    ) {
+      acc.push(`${space} - ${key}: ${parsedFile1[key]}`);
+    } else if (
+      parsedFile1[key] === undefined
+      && parsedFile2[key] !== undefined
+    ) {
+      acc.push(`${space} + ${key}: ${parsedFile2[key]}`);
+    } else {
+      acc.push(`${space} - ${key}: ${parsedFile1[key]}`);
+      acc.push(`${space} + ${key}: ${parsedFile2[key]}`);
+    }
+    return acc;
+  }, []);
+  return `{\n${diff.join('\n')}\n}`;
+};
