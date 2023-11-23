7ef1e31cb689b74cffdb0d509fd62836f81c9124
Karina Timofeeva <kar.timofeeva93@gmail.com>
2023-11-23 15:16:17 +0100

    Updated __tests__/index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,8 +1,8 @@
-import { test, expect, describe } from '@jest/globals';
-import { fileURLToPath } from 'url';
-import path, { dirname } from 'path';
-import { readFileSync } from 'fs';
-import { compareTwoJSONFiles } from '../src/index.js';
+import { test, expect, describe } from "@jest/globals";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+import { readFileSync } from "fs";
+import { generateDiff } from "../src/index.js";
 
 // convert this file URL to a file path compatible with Node.js functions
 // __filename is the absolute path to the current file:
@@ -13,17 +13,27 @@ const __dirname = dirname(__filename);
 
 // абсолютные пути, созданные с помощью, getFixturePath будут указывать на файлы
 // в __fixtures__ директории относительно местоположения index.test.js.
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const getFixturePath = (filename) =>
+  path.join(__dirname, "..", "__fixtures__", filename);
 
-const readContentOfFixture = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+const readContentOfFixture = (filename) =>
+  readFileSync(getFixturePath(filename), "utf-8");
 
-describe('JSON Files Comparison', () => {
-  const file1Path = getFixturePath('file1.json');
-  const file2Path = getFixturePath('file2.json');
+describe("JSON and YAML Files Comparison", () => {
+  const file1JSONPath = getFixturePath("file1.json");
+  const file2JSONPath = getFixturePath("file2.json");
+  const file1YMLPath = getFixturePath("file1.yml");
+  const file2JYAMLPath = getFixturePath("file2.yaml");
 
-  test('compareTwoJSONFiles', () => {
-    const result = compareTwoJSONFiles(file1Path, file2Path);
-    const expected = readContentOfFixture('result_of_compare.txt').trim();
+  test("compareTwoJSONFiles", () => {
+    const result = generateDiff(file1JSONPath, file2JSONPath);
+    const expected = readContentOfFixture("result_of_compare.txt").trim();
+    expect(result.trim()).toEqual(expected);
+  });
+
+  test("compareTwoJSONFiles", () => {
+    const result = generateDiff(file1YMLPath, file2JYAMLPath);
+    const expected = readContentOfFixture("result_of_compare.txt").trim();
     expect(result.trim()).toEqual(expected);
   });
 });
