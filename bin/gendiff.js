82f7d6a53dae88400476aa12c6ef5cd9539f2c20
Karina Timofeeva <kar.timofeeva93@gmail.com>
2023-11-23 15:26:56 +0100

    Updated bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
-import { program } from "commander";
-import { generateDiff } from "../src/index.js";
+import { program } from 'commander';
+import { generateDiff } from '../src/index.js';
 
 program
-  .description("Compares two configuration files and shows a difference.")
-  .arguments("<filepath1> <filepath2>")
-  .option("-f, --format <type>", "output format")
-  .option("-V, --version", "output the version number")
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
+  .option('-V, --version', 'output the version number')
   .action((filepath1, filepath2) => {
     const diff = generateDiff(filepath1, filepath2);
     console.log(diff);
