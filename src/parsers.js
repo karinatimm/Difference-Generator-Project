c6d9b170c6e4a5e4a8f0ce0ff1cfdb9b5137a2db
Karina Timofeeva <kar.timofeeva93@gmail.com>
2023-11-23 15:14:47 +0100

    Added src/parsers.js

---
+++ src/parsers.js
@@ -0,0 +1,18 @@
+import yaml from "js-yaml";
+import { readAndGetContentOfFile, getExtension } from "./index.js";
+
+// convert JSON-formatted and YAML-formatted strings into JS object:
+export const parseFileContent = (filepath) => {
+  const extension = getExtension(filepath);
+  const contentOfFile = readAndGetContentOfFile(filepath);
+
+  switch (extension) {
+    case "json":
+      return JSON.parse(contentOfFile);
+    case "yml":
+    case "yaml":
+      return yaml.load(contentOfFile);
+    default:
+      throw new Error(`Invalid extension: ${extension}`);
+  }
+};
