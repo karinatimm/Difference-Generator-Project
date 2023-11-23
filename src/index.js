e57d57020ceb0b5daf457169755c052442ca6c02
Karina Timofeeva <kar.timofeeva93@gmail.com>
2023-11-23 15:18:01 +0100

    Updated src/index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,27 @@
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
+import { parseFileContent } from "./parsers.js";
 
 // convert filepath into an absolute path based on the current working directory:
-export const getAbsolutePathToFile = (filepath) => {
-  const pathToFile = path.resolve(process.cwd(), filepath);
-  return pathToFile;
-};
+export const getAbsolutePathToFile = (filepath) =>
+  path.resolve(process.cwd(), filepath);
+
 // read the content of the file at the end of this absolute path:
-export const readTheContentOfJSONFile = (filepath) => {
+export const readAndGetContentOfFile = (filepath) => {
   const absolutePathToFile = getAbsolutePathToFile(filepath);
-  const content = fs.readFileSync(absolutePathToFile);
-  return content;
-};
-// convert JSON-formatted string into JS object:
-export const parseJSONFileIntoJSObject = (filepath) => {
-  const contentOfJSONFile = readTheContentOfJSONFile(filepath);
-  const parsedFile = JSON.parse(contentOfJSONFile);
-  return parsedFile;
+  return fs.readFileSync(absolutePathToFile);
 };
 
-export const generateJSONDiff = (
-  parsedFile1,
-  parsedFile2,
-  replacer,
-  spacesCount,
-) => {
+// path.extname method returns the extension (including the dot)
+export const getExtension = (filepath) => path.extname(filepath).slice(1);
+
+export const compareFiles = (filepath1, filepath2, replacer, spacesCount) => {
+  const content1 = readAndGetContentOfFile(filepath1);
+  const content2 = readAndGetContentOfFile(filepath2);
+
+  const parsedFile1 = parseFileContent(content1);
+  const parsedFile2 = parseFileContent(content2);
+
   const keys1 = Object.keys(parsedFile1);
   const keys2 = Object.keys(parsedFile2);
   const unitKeys = [...new Set([...keys1, ...keys2])].sort();
@@ -50,20 +47,12 @@ export const generateJSONDiff = (
   }, []);
 };
 
-export const compareTwoJSONFiles = (
+export const generateDiff = (
   filepath1,
   filepath2,
-  replacer = ' ',
-  spacesCount = 1,
+  replacer = " ",
+  spacesCount = 1
 ) => {
-  const parsedFile1 = parseJSONFileIntoJSObject(filepath1);
-  const parsedFile2 = parseJSONFileIntoJSObject(filepath2);
-
-  const diff = generateJSONDiff(
-    parsedFile1,
-    parsedFile2,
-    replacer,
-    spacesCount,
-  );
-  return `{\n${diff.join('\n')}\n}`;
+  const diff = compareFiles(filepath1, filepath2, replacer, spacesCount);
+  return `{\n${diff.join("\n")}\n}`;
 };
