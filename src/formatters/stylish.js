1c5126bae443dfe272da3ca7a562a6dbe0322271
Karina Timofeeva <kar.timofeeva93@gmail.com>
2023-11-25 18:05:36 +0100

    Updated src/formatters/stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,81 +1,84 @@
-import _ from "lodash";
+import _ from 'lodash';
 
-const replacer = " ";
-const indentationForClosingBraces = "  ";
-const spacesCount = 4;
-const shiftToTheLeft = 2;
+const getIndentNested = (nestingLevel) => {
+  const replacer = ' ';
+  const spacesCount = 4;
+  const shiftToTheLeft = 2;
+  const indent = replacer.repeat(nestingLevel * spacesCount - shiftToTheLeft);
+  return indent;
+};
 
-// calculate the total number of spaces for indentation at a particular nesting level
-const getIndent = (nestingLevel) =>
-  replacer.repeat(nestingLevel * spacesCount - shiftToTheLeft);
+const getIndentSimple = (nestingLevel) => {
+  const replacer = ' ';
+  const spacesCount = 4;
+  const indent = replacer.repeat(nestingLevel * spacesCount);
+  return indent;
+};
 
 // check if it the value of object is nested or not
-const stringifyValueOfObject = (value, nestingLevel) => {
+const stringify = (value, nestingLevel) => {
   // if it's not an object
   if (!_.isPlainObject(value)) {
     return String(value);
   }
-  //check children
+  // check children
   // array containing formatted strings for each key-value pair within the object
-  const arrOfFormattedKeyValueString = Object.entries(value).map(
-    ([keyOfValue, valOfValue]) => {
-      // total indentation spaces based on the nesting level
-      const totalNumOfIndentationSpaces = getIndent(nestingLevel + 1);
-      // construct a formatted string for each key-value pair in the object
-      return `${totalNumOfIndentationSpaces}  ${keyOfValue}: ${stringifyValueOfObject(
-        valOfValue,
-        nestingLevel + 1
-      )}`;
-    }
+  const arrOfFormattedKeyValStrings = Object.entries(value).map(
+    ([keyOfValue, valOfValue]) => `${getIndentSimple(nestingLevel + 1)}${keyOfValue}: ${stringify(
+      valOfValue,
+      nestingLevel + 1,
+    )}\n`,
   );
-  // construct the final formatted string (the entire object and its children)
-  return `{\n${arrOfFormattedKeyValueString.join("\n")}\n${getIndent(
-    nestingLevel
-  )}${indentationForClosingBraces}}`;
+
+  return `{\n${arrOfFormattedKeyValStrings.join('')}${getIndentSimple(
+    nestingLevel,
+  )}}`;
 };
 
 const stylish = (diffTreeOfFiles, nestingLevel = 1) => {
-  const arrOfFormattedPairStrings = diffTreeOfFiles.map(
-    ({ key, type, value, valueInFile1, valueInFile2 }) => {
+  const formattedDiffOfTree = diffTreeOfFiles.map(
+    ({
+      key, type, value, valueInFile1, valueInFile2,
+    }) => {
       switch (type) {
-        case "nested":
-          return `${getIndent(nestingLevel)}  ${key}: {\n${stylish(
+        case 'nested':
+          return `${getIndentNested(nestingLevel)}  ${key}: ${stylish(
             value,
-            nestingLevel + 1
-          )}\n${getIndent(nestingLevel)}${indentationForClosingBraces}}`;
-        case "addMinusForFile1":
-          return `${getIndent(nestingLevel)}- ${key}: ${stringifyValueOfObject(
-            value,
-            nestingLevel
+            nestingLevel + 1,
           )}`;
-        case "addPlusForFile2":
-          return `${getIndent(nestingLevel)}+ ${key}: ${stringifyValueOfObject(
+        case 'addMinusForFile1':
+          return `${getIndentNested(nestingLevel)}- ${key}: ${stringify(
             value,
-            nestingLevel
+            nestingLevel,
           )}`;
-        case "addedBoth":
-          return `${getIndent(nestingLevel)}- ${key}: ${stringifyValueOfObject(
+        case 'addPlusForFile2':
+          return `${getIndentNested(nestingLevel)}+ ${key}: ${stringify(
+            value,
+            nestingLevel,
+          )}`;
+        case 'addBoth':
+          return `${getIndentNested(nestingLevel)}- ${key}: ${stringify(
             valueInFile1,
-            nestingLevel
-          )}\n${getIndent(nestingLevel)}+ ${key}: ${stringifyValueOfObject(
+            nestingLevel,
+          )}\n${getIndentNested(nestingLevel)}+ ${key}: ${stringify(
             valueInFile2,
-            nestingLevel
+            nestingLevel,
           )}`;
-        case "unchanged":
-          return `${getIndent(nestingLevel)}  ${key}: ${stringifyValueOfObject(
+        case 'unchanged':
+          return `${getIndentNested(nestingLevel)}  ${key}: ${stringify(
             value,
-            nestingLevel
+            nestingLevel,
           )}`;
         default:
           throw new Error(
-            `Error: ${type} - this type doesn't exist in this file`
+            `Error: "${type}" - this type doesn't exist in this file`,
           );
       }
-    }
+    },
   );
-  //   console.log(arrOfFormattedPairStrings);
-  const formattedDiffTree = `{\n${arrOfFormattedPairStrings.join("\n")}\n}`;
-  return formattedDiffTree;
+  return `{\n${formattedDiffOfTree.join('\n')}\n${getIndentSimple(
+    nestingLevel - 1,
+  )}}`;
 };
 
 export default stylish;
