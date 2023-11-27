9cd0137bb2aaf4d983a6156cfc0028c7d7a53926
Karina Timofeeva <kar.timofeeva93@gmail.com>
2023-11-27 19:09:37 +0100

    Updated src/buildDiffTree.js

--- src/buildDiffTree.js
+++ src/buildDiffTree.js
@@ -3,39 +3,70 @@ import _ from 'lodash';
 const buildDiffTree = (parsedFile1, parsedFile2) => {
   const keys1 = Object.keys(parsedFile1);
   const keys2 = Object.keys(parsedFile2);
-  const allKeys = _.union(keys1, keys2);
-  const sortedKeys = _.sortBy(allKeys);
+  const allUnionKeys = _.union(keys1, keys2);
+  const sortedUnionKeys = _.sortBy(allUnionKeys);
 
-  const diff = sortedKeys.reduce((acc, key) => {
-    const currentValue1 = parsedFile1[key];
-    const currentValue2 = parsedFile2[key];
+  const arrOfDiffTree = sortedUnionKeys.map((key) => {
+    const currVal1 = parsedFile1[key];
+    const currVal2 = parsedFile2[key];
 
-    // nested
-    if (_.isPlainObject(currentValue1) && _.isPlainObject(currentValue2)) {
-      acc.push({
+    if (_.isPlainObject(currVal1) && _.isPlainObject(currVal2)) {
+      return {
         key,
-        value: buildDiffTree(currentValue1, currentValue2),
+        value: buildDiffTree(currVal1, currVal2),
         type: 'nested',
-      });
-    } else if (_.isEqual(currentValue1, currentValue2)) {
-      acc.push({ key, value: currentValue1, type: 'unchanged' });
-    } else if (_.isUndefined(currentValue1)) {
-      acc.push({ key, value: currentValue2, type: 'addPlusForFile2' });
-    } else if (_.isUndefined(currentValue2)) {
-      acc.push({ key, value: currentValue1, type: 'addMinusForFile1' });
-    } else {
-      acc.push({
-        key,
-        valueInFile1: currentValue1,
-        valueInFile2: currentValue2,
-        type: 'addBoth',
-      });
+      };
     }
 
-    return acc;
-  }, []);
+    if (_.isEqual(currVal1, currVal2)) {
+      return { key, value: currVal1, type: 'unchanged' };
+    }
 
-  return diff;
+    if (_.isUndefined(currVal1)) {
+      return { key, value: currVal2, type: 'addPlusForFile2' };
+    }
+
+    if (_.isUndefined(currVal2)) {
+      return { key, value: currVal1, type: 'addMinusForFile1' };
+    }
+
+    return {
+      key,
+      value1: currVal1,
+      value2: currVal2,
+      type: 'addBoth',
+    };
+  });
+
+  return arrOfDiffTree;
 };
 
+// console.log(buildDiffTree(file1, file2));
 export default buildDiffTree;
+
+// nested
+//     if (_.isPlainObject(currVal1) && _.isPlainObject(currVal2)) {
+//       acc.push({
+//         key,
+//         value: buildDiffTree(currVal1, currVal2),
+//         type: "nested",
+//       });
+//     } else if (_.isEqual(currVal1, currVal2)) {
+//       acc.push({ key, value: currVal1, type: "unchanged" });
+//     } else if (_.isUndefined(currVal1)) {
+//       acc.push({ key, value: currVal2, type: "addPlusForFile2" });
+//     } else if (_.isUndefined(currVal2)) {
+//       acc.push({ key, value: currVal1, type: "addMinusForFile1" });
+//     } else {
+//       acc.push({
+//         key,
+//         value1: currVal1,
+//         value2: currVal2,
+//         type: "addBoth",
+//       });
+//     }
+
+//     return acc;
+//   }, []);
+
+//   return diff;
