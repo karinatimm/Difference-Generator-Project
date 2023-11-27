70fe648a1781407da6a0d2b07df870690886d878
Karina Timofeeva <kar.timofeeva93@gmail.com>
2023-11-27 20:16:09 +0100

    Updated src/buildDiffTree.js

--- src/buildDiffTree.js
+++ src/buildDiffTree.js
@@ -6,41 +6,36 @@ const buildDiffTree = (parsedFile1, parsedFile2) => {
   const allUnionKeys = _.union(keys1, keys2);
   const sortedUnionKeys = _.sortBy(allUnionKeys);
 
-  const arrOfDiffTree = sortedUnionKeys.map((key) => {
+  const arrOfDiffStrings = sortedUnionKeys.reduce((acc, key) => {
     const currVal1 = parsedFile1[key];
     const currVal2 = parsedFile2[key];
 
     if (_.isPlainObject(currVal1) && _.isPlainObject(currVal2)) {
-      return {
+      acc.push({
         key,
         value: buildDiffTree(currVal1, currVal2),
         type: 'nested',
-      };
+      });
+    } else if (_.isEqual(currVal1, currVal2)) {
+      acc.push({ key, value: currVal1, type: 'unchanged' });
+    } else if (_.isUndefined(currVal1)) {
+      acc.push({ key, value: currVal2, type: 'addPlusForFile2' });
+    } else if (_.isUndefined(currVal2)) {
+      acc.push({ key, value: currVal1, type: 'addMinusForFile1' });
+    } else {
+      acc.push({
+        key,
+        value1: currVal1,
+        value2: currVal2,
+        type: 'addBoth',
+      });
     }
 
-    if (_.isEqual(currVal1, currVal2)) {
-      return { key, value: currVal1, type: 'unchanged' };
-    }
+    return acc;
+  }, []);
 
-    if (_.isUndefined(currVal1)) {
-      return { key, value: currVal2, type: 'addPlusForFile2' };
-    }
-
-    if (_.isUndefined(currVal2)) {
-      return { key, value: currVal1, type: 'addMinusForFile1' };
-    }
-
-    return {
-      key,
-      value1: currVal1,
-      value2: currVal2,
-      type: 'addBoth',
-    };
-  });
-
-  return arrOfDiffTree;
+  return arrOfDiffStrings;
 };
-
 // console.log(buildDiffTree(file1, file2));
 export default buildDiffTree;
 
